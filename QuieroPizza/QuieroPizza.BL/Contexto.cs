4946af177c62a08a0344f43e38845c01bebebf31
Eriko Daniel Cruz <eriko.cruz@unah.hn>
2022-02-01 14:36:59 -0600

    Creando Contexto y DBbset de productos con el Entity Framework

---
+++ QuieroPizza/QuieroPizza.BL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuieroPizza.BL
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(): base("QuieroPizzaDB")
+        {
+                
+        }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
