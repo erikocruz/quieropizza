9f8b2617cefddfeaca51fc8f8aee7647bea1897f
Eriko Daniel Cruz <eriko.cruz@unah.hn>
2022-02-08 11:12:48 -0600

    Agregando Emtity Framework Code First

--- QuieroPizza/QuieroPizza.BL/Contexto.cs
+++ QuieroPizza/QuieroPizza.BL/Contexto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,15 @@ namespace QuieroPizza.BL
 {
     public class Contexto: DbContext
     {
-        public Contexto(): base("QuieroPizzaDB")
+        public Contexto(): base(@"Data Source=(LocalDb)\MSSQLLocalDB;AttachDBFilename=D:\_Proyectos\MisBDs\QuieroPizzaDB.mdf") //'+
+        //  //  Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\QuieroPizzaDB.mdf")
         {
-                
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
         }
         public DbSet<Producto> Productos { get; set; }
     }
--- QuieroPizza/QuieroPizza.BL/ProductosBL.cs
+++ QuieroPizza/QuieroPizza.BL/ProductosBL.cs
@@ -9,38 +9,42 @@ namespace QuieroPizza.BL
     public class ProductosBL
     {
         Contexto _contexto;
+        public List<Producto> ListadeProductos { get; set; }
 
         public ProductosBL()
         {
             _contexto = new Contexto();
-
+            ListadeProductos = new List<Producto>();
         }
 
         public List<Producto> ObtenerProductos()
         {
-            _contexto.Productos.ToList();
+            ListadeProductos = _contexto.Productos.ToList();
 
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Pizza 6 Quesos";
-            producto1.Precio = 200;
+            return ListadeProductos;
 
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Pizza 4 Estaciones";
-            producto2.Precio = 250;
+            //var producto1 = new Producto();
+            //producto1.Id = 1;
+            //producto1.Descripcion = "Pizza 6 Quesos";
+            //producto1.Precio = 200;
 
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Pizza Jamón y Queso";
-            producto3.Precio = 180;
+            //var producto2 = new Producto();
+            //producto2.Id = 2;
+            //producto2.Descripcion = "Pizza 4 Estaciones";
+            //producto2.Precio = 250;
 
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-            
-            return listadeProductos;
+            //var producto3 = new Producto();
+            //producto3.Id = 3;
+            //producto3.Descripcion = "Pizza Jamón y Queso";
+            //producto3.Precio = 180;
+
+            //var listadeProductos = new List<Producto>();
+            //listadeProductos.Add(producto1);
+            //listadeProductos.Add(producto2);
+            //listadeProductos.Add(producto3);
+
+            //return listadeProductos;
+
         }
     }
 }
